824c32b52a1d331e5a29263a9dcb1750203c823a
zolcsika71 <bushdoctor2008@gmail.com>
2021-06-24 21:49:02 +0200

    day

--- Duplicate File Handler/Topics/Scopes/Hero damage/main.py
+++ Duplicate File Handler/Topics/Scopes/Hero damage/main.py
@@ -2,15 +2,16 @@ hero_damage = 100
 
 
 def double_damage():
+    global hero_damage
     hero_damage = hero_damage * 2
 
 
 def disarmed():
+    global hero_damage
     hero_damage = hero_damage * 0.1
 
 
 def power_potion():
-    pass
+    global hero_damage
+    hero_damage = hero_damage + 100
 
-
-double_damage()
